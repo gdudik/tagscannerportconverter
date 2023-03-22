a33623b99ade014e2a5b1e784af9360677b1812c
Graham Dudik <graham.dudik@athletic.net>
2023-03-22 12:04:05 -0700

    change to com25

--- tagscannerportconverter.js
+++ tagscannerportconverter.js
@@ -2,7 +2,7 @@ const { SerialPort }= require('serialport');
 const {ReadlineParser} = require('@serialport/parser-readline')
 const prompt = require('prompt');
 
-const port = new SerialPort({ path: 'COM5', baudRate: 9600});
+const port = new SerialPort({ path: 'COM25', baudRate: 9600});
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 parser.on('data', console.log)
